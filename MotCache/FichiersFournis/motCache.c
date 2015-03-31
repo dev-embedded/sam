bccafca9029c218d0eb4cd1d68f93eee27c17464
SamRoot <SamRoot@PC-201501121519>
2015-03-30 23:46:30 -0400

    add files to MotCache

---
+++ MotCache/FichiersFournis/motCache.c
@@ -0,0 +1,10 @@
+#include "util.h"
+
+// Compilation : gcc -Wall -std=c99 main.c util.c
+// Ceci est un exemple d'utilisation de util.c
+
+
+int main (int argc, char const *argv[])
+{
+        print_position_mot("bien",7,9,NE);
+}
